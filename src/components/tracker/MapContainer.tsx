97d9a16940cd73d496c65909ff0057401fb9fb05
신용준 <yjshin.unimewo@gmail.com>
2025-03-31 08:49:05 +0900

    feat: 결과 화면 지도 zoom 계산

--- src/components/tracker/MapContainer.tsx
+++ src/components/tracker/MapContainer.tsx
@@ -2,31 +2,45 @@
 
 import { color } from '@/styles/variable';
 import { TLatLng } from '@/types/tracker';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, Polyline, TileLayer } from 'react-leaflet';
+import { useEffect } from 'react';
+import { MapContainer, Polyline, TileLayer, useMap } from 'react-leaflet';
 
 interface IProps {
   segments: TLatLng[][];
 }
 
-// 좌표의 중심값 구하기
-const getCenterFromSegments = (segments: TLatLng[][]): TLatLng => {
+// 모든 좌표를 flat하게 모아서 bounds 계산
+const getBoundsFromSegments = (segments: TLatLng[][]): L.LatLngBounds => {
   const allPoints = segments.flat();
-  if (allPoints.length === 0) return [37.5665, 126.978];
+  return L.latLngBounds(allPoints);
+};
 
-  const [latSum, lngSum] = allPoints.reduce(
-    ([latAcc, lngAcc], [lat, lng]) => [latAcc + lat, lngAcc + lng],
-    [0, 0],
-  );
+// Bounds 적용 컴포넌트
+const FitBounds = ({ segments }: { segments: TLatLng[][] }) => {
+  const map = useMap();
+  const bounds = getBoundsFromSegments(segments);
 
-  const count = allPoints.length;
-  return [latSum / count, lngSum / count];
+  // bounds 적용
+  useEffect(() => {
+    if (segments.flat().length > 0) {
+      map.fitBounds(bounds, {
+        padding: [20, 20], // 여백 추가
+        maxZoom: 17, // 너무 확대되지 않도록 제한
+      });
+    }
+  }, [segments, map, bounds]);
+
+  return null;
 };
 
 export default function MapContainerNoSSR({ segments }: IProps) {
+  const defaultCenter: TLatLng = [37.5665, 126.978]; // fallback center
+
   return (
     <MapContainer
-      center={getCenterFromSegments(segments)}
+      center={defaultCenter}
       zoom={16}
       style={{ height: 300 }}
       zoomControl={false} // + / - 버튼 제거
@@ -36,6 +50,7 @@ export default function MapContainerNoSSR({ segments }: IProps) {
       dragging={false}
     >
       <TileLayer url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png" />
+      <FitBounds segments={segments} />
       {segments.map((segment, i) =>
         segment.length > 1 ? (
           <Polyline key={i} positions={segment} color={color.primary} />
