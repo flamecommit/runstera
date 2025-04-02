373c033e7fee5df0f761cbd1a569d21027086c2f
flamecommit <shinyj1991@gmail.com>
2025-04-02 13:19:18 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ function TokenLoginHandler() {
     if (token) {
       signIn('credentials', {
         token,
-        callbackUrl: '/', // 로그인 후 이동할 경로
+        callbackUrl: '/tracker', // 로그인 후 이동할 경로
       });
     }
   }, [token]);
