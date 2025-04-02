cf476fd2c3b366665feb9e64a71e0b40f9deed8a
flamecommit <shinyj1991@gmail.com>
2025-04-02 14:04:41 +0900

    set middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,27 +4,24 @@ import { NextResponse } from 'next/server';
 
 export default async function middleware(req: NextRequest) {
   // const token = await getToken({ req });
-  // const { pathname } = req.nextUrl;
-
-  // const exactPublicPaths = ['/'];
-  // const prefixPublicPaths = ['/signin'];
-
-  // const isExactPublic = exactPublicPaths.includes(pathname);
-  // const isPrefixPublic = prefixPublicPaths.some((path) =>
-  //   pathname.startsWith(path),
-  // );
-
-  // if ((isExactPublic || isPrefixPublic) && token) {
-  //   return NextResponse.redirect(new URL('/tracker', req.url));
-  // }
-
-  // if (!token && !isExactPublic && !isPrefixPublic) {
-  //   return NextResponse.redirect(new URL('/', req.url));
-  // }
+  const { pathname } = req.nextUrl;
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
   const response = NextResponse.next();
-  if (token) {
+
+  const exactPublicPaths = ['/'];
+  const prefixPublicPaths = ['/signin'];
+
+  const isExactPublic = exactPublicPaths.includes(pathname);
+  const isPrefixPublic = prefixPublicPaths.some((path) =>
+    pathname.startsWith(path),
+  );
+
+  if (isExactPublic && token) {
+    return NextResponse.redirect(new URL('/tracker', req.url));
+  }
+
+  if (isPrefixPublic) {
     response.headers.set('x-access-token', token?.accessToken as string);
   }
 
