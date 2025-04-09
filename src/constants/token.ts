2b3c8e3cc07263ec68862771ad952aad344eba7c
flamecommit <shinyj1991@gmail.com>
2025-04-09 19:29:30 +0900

    feat: accessToken 10초, refreshToken 30일

--- src/constants/token.ts
+++ src/constants/token.ts
@@ -1,2 +1,2 @@
-export const ACCESS_TOKEN_EXPIRY = '15m';
-export const REFRESH_TOKEN_EXPIRY = '7d';
+export const ACCESS_TOKEN_EXPIRY = 10;
+export const REFRESH_TOKEN_EXPIRY = 60 * 60 * 24 * 30; // 30일
